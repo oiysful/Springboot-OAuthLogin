530ac52dcdfb049a622013140ff21a21ad64a909
이용상 <gaonoiys@gmail.com>
2023-04-21 09:34:56 +0900

    2023.04.21. First Commit
    - Springboot Project(Gradle, yml)

---
+++ Springboot-OAuthLogin/src/main/java/com/ian/SpringbootOAuthLogin/SpringbootOAuthLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ian.SpringbootOAuthLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOAuthLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOAuthLoginApplication.class, args);
+	}
+
+}
---
+++ Springboot-OAuthLogin/src/test/java/com/ian/SpringbootOAuthLogin/SpringbootOAuthLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.SpringbootOAuthLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOAuthLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
